0054618050402f6ea4a88a190738271d23be855c
Yanglin-Tao <81639920+Yanglin-Tao@users.noreply.github.com>
2023-10-29 23:01:33 -0400

    Define strong and weak relationship

--- JavaWebCrawler/src/webcrawler/SeleniumCrawler.java
+++ JavaWebCrawler/src/webcrawler/SeleniumCrawler.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
@@ -20,15 +21,23 @@ public class SeleniumCrawler {
     private final String keyword;
     private final int maxPages;
     private final Set<String> visitedUrls = new HashSet<>();
-    private final Set<String> articleTitles = new HashSet<>();
+    private final Set<String> totalArticleTitles = new HashSet<>();
+    private final Set<String> strongRelationshipArticleTitles = new HashSet<>();
+    private final Set<String> weakRelationshipArticleTitles = new HashSet<>();
     private final ExecutorService executorService;
+    private ArrayList<String> strongRelationKeywordList = new ArrayList<String>();
+    private ArrayList<String> weakRelationKeywordList = new ArrayList<String>();
 
     public SeleniumCrawler(String rootUrl, String keyword, int numThreads, int maxPages) {
         this.rootUrl = rootUrl;
         this.keyword = keyword;
         this.executorService = Executors.newFixedThreadPool(numThreads);
         this.maxPages = maxPages;
-
+        strongRelationKeywordList.add("security");
+        strongRelationKeywordList.add("defense");
+        weakRelationKeywordList.add("change");
+        weakRelationKeywordList.add("risk");
+        
         System.setProperty("webdriver.chrome.driver", "/opt/homebrew/bin/chromedriver");
 
     }
@@ -45,11 +54,22 @@ public class SeleniumCrawler {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.println("Article titles: " + articleTitles);
-        for (String title : articleTitles) {
+        System.out.println("DONE. Finished parsing " + maxPages + " pages");
+        for (String title : totalArticleTitles) {
         	System.out.println(title);
         }
-        System.out.println("DONE. Number of articles found in " + maxPages + " pages: " + articleTitles.size());
+        System.out.println("Number of articles with weak or strong relationship found: " + totalArticleTitles.size());
+        System.out.println("---------------------------------------------------------------------------------------");
+        for (String title : strongRelationshipArticleTitles) {
+        	System.out.println(title);
+        }
+        System.out.println("Number of articles with strong relationship found: " + strongRelationshipArticleTitles.size());
+        System.out.println("---------------------------------------------------------------------------------------");
+        for (String title : weakRelationshipArticleTitles) {
+        	System.out.println(title);
+        }
+        System.out.println("Number of articles with weak relationship found: " + weakRelationshipArticleTitles.size());
+        System.out.println("---------------------------------------------------------------------------------------");
     }
 
     private void crawl(String url) {
@@ -66,13 +86,19 @@ public class SeleniumCrawler {
                 String htmlContent = driver.getPageSource();
                 Document doc = Jsoup.parse(htmlContent);
 
-                System.out.println(doc.title());
+                // System.out.println(doc.title());
 
                 Elements newsTitles = doc.select("a[data-ga4-ecommerce-path^=/government/news/]");
                 for (Element title : newsTitles) {
-                    synchronized (articleTitles) {
-                    	if (title.text().toLowerCase().contains(keyword)) {
-                    		articleTitles.add(title.text());
+                    synchronized (totalArticleTitles) {
+                    	if (title.text().toLowerCase().contains(keyword) && (isStrongRelationship(title.text().toLowerCase()) || (isWeakRelationship(title.text().toLowerCase())))) {
+                    		totalArticleTitles.add(title.text());
+                    	} 
+                    	if (title.text().toLowerCase().contains(keyword) && isStrongRelationship(title.text().toLowerCase())) {
+                    		strongRelationshipArticleTitles.add(title.text());
+                    	} 
+                    	if (title.text().toLowerCase().contains(keyword) && isWeakRelationship(title.text().toLowerCase())) {
+                    		weakRelationshipArticleTitles.add(title.text());
                     	}
                     }
                 }
@@ -83,9 +109,27 @@ public class SeleniumCrawler {
             }
         });
     }
+    
+    private Boolean isStrongRelationship(String titleText) {
+    	for (String keyword : strongRelationKeywordList) {
+    		if (titleText.contains(keyword)) {
+    			return true;
+    		}
+    	}
+    	return false;
+    }
+    
+	private Boolean isWeakRelationship(String titleText) {
+		for (String keyword : weakRelationKeywordList) {
+    		if (titleText.contains(keyword)) {
+    			return true;
+    		}
+    	}
+	    return false;
+	}
 
     public static void main(String[] args) {
-        SeleniumCrawler crawler = new SeleniumCrawler("https://www.gov.uk/search/news-and-communications", "climate", 1000, 40);
+        SeleniumCrawler crawler = new SeleniumCrawler("https://www.gov.uk/search/news-and-communications", "climate", 1000, 150);
         crawler.start();
     }
 }
